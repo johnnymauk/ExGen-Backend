35d5b63a96263709d0f4fe822b95caef168a409d
Johnny Mauk <career@jrmauk.com>
2021-03-23 09:46:42 -0500

    Wrote more routes and tests. Will likely have to fix a few things up later. Need to think about endpoints a bit more. Plan etc.

--- routes/account/account.js
+++ routes/account/account.js
@@ -2,16 +2,53 @@ const express = require("express");
 const router = express.Router();
 const { db } = require("../../db");
 
-router.route("/")
-    .post((req, res) =>
-        res.status(200).json({message:"POST ACCOUNT"})
-    );
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const secret = 'mubble';
+
+router.route("/check")
+    .get(async (req, res) => {
+        if(req.query.email !== null){
+            await db
+                .query("SELECT COUNT(account_id) FROM account WHERE email=$1 LIMIT 1",[req.query.email])
+                .then(obj => {
+                    if(parseInt(obj.rows[0].count) === 1){
+                        res.status(200).json({info: 1})
+                    }else{
+                        res.status(404).json({info: 0})
+                    }
+                })
+        }else{
+            res.status(400).json({info: undefined})
+        }
+    });
+
+router.route("/create")
+    .post(async (req, res) => {
+        if(
+            req.body.fname !== null && req.body.lname !== null && req.body.email !== null && req.body.pass !==null
+        ){
+            await db
+                .query("SELECT * FROM _account_c($1,$2,$3,$4)",[req.body.email, req.body.fname, req.body.lname, req.body.pass])
+                .then(obj => {
+                    res.status(obj.rowCount === 0 ? 404:200).json({info: obj.rows[0]})
+                })
+                .catch(err => {
+                    console.error(err.stack);
+                    res.status(500).json({info: undefined})
+                })
+        }else{
+            res.status(400).json({info: undefined})
+        }
+    });
 
 router.route("/:account_id")
+
+    // GETS ACCOUNT INFORMATION
     .get(async (req, res) => {
         if(Number.isInteger(parseInt(req.params.account_id))){
             await db
-                .query("SELECT * from _account_r($1);", [req.params.account_id])
+                .query("SELECT * FROM _account_r($1);", [req.params.account_id])
                 .then( obj => {
                     res.status(obj.rowCount === 0 ? 404:200).json({info: obj.rows[0]})
                 })
@@ -23,5 +60,63 @@ router.route("/:account_id")
             res.status(400).json({info: undefined})
         }
     })
+
+    // CHANGES TO EVERYTHING EXCEPT PASSWORD
+    .post(async (req, res) => {
+        if(
+            Number.isInteger(parseInt(req.params.account_id)) &&
+            req.body.fname !== null && req.body.lname !== null && req.body.email !== null
+        ){
+            await db
+                .query("SELECT * FROM _account_u($1,$2,$3,$4)",[req.params.account_id, req.body.email, req.body.fname, req.body.lname])
+                .then(obj => {
+                    res.status(obj.rowCount === 0 ? 404:200).json({info: obj.rows[0]})
+                })
+                .catch(err => {
+                    console.error(err.stack);
+                    res.status(500).json({info: undefined})
+                })
+        }else{
+            res.status(400).json({info: undefined})
+        }
+    });
+
+router.route("/:account_id/password_reset")
+
+    // PASSWORD CHANGES
+    .post(async (req, res) => {
+        if(Number.isInteger(parseInt(req.params.account_id)) && req.body.hash == null){
+            await bcrypt.hash(req.body.hash + secret, saltRounds).then( async hash => {
+                await db
+                    .query('SELECT * FROM _account_u_password($1,$2);',[req.params.account_id, hash])
+                    .then(obj => {
+                        res.status(obj.rowCount === 0 ? 404:200).json({info: obj.rows[0]})
+                    })
+                    .catch(err =>{
+                        console.error(err.stack);
+                        res.status(500).json({info: undefined})
+                    })
+            })
+        }else{
+            res.status(400).json({info: undefined})
+        }
+    });
+
+router.route('/:account_id/delete')
+    .delete( async (req, res) => {
+        if(Number.isInteger(parseInt(req.params.account_id))){
+            await db
+                .query('SELECT * FROM _account_d($1);',[req.params.account_id])
+                .then(obj => {
+                    res.status(obj.rowCount === 0 ? 404:200).json({info: obj.rows[0]})
+                })
+                .catch(err =>{
+                    console.error(err.stack);
+                    res.status(500).json({info: undefined})
+                })
+        }else{
+            res.status(400).json({info: undefined})
+        }
+    });
 
 module.exports = router;
--- routes/account/acount.test.js
+++ routes/account/acount.test.js
@@ -7,7 +7,7 @@ afterAll(async () => {
     await db.end();
 });
 
-describe('ACCOUNT::GET', () => {
+describe('ACCOUNT::GET::ID', () => {
 
     test('GOOD', async done => {
         await request.get('/account/1').then( response => {
@@ -36,11 +36,120 @@ describe('ACCOUNT::GET', () => {
     });
 })
 
+describe('ACCOUNT::POST::ID', () =>{
+    const data = {
+        email: "example2@example.com",
+        fname: "Mazie",
+        lname: "Nicky"
+    }
 
-test('Account::POST', async done => {
-    await request.post('/account').then( response => {
-        expect(response.status).toBe(200);
-        expect(response.body.message).toBe("POST ACCOUNT");
+    test('GOOD', async done => {
+        await request
+            .post('/account/2')
+            .send(data)
+            .then( response => {
+                expect(response.status).toBe(200);
+                expect(response.body.info).toBeDefined();
+                expect(response.body.info.account_id).toBe(2);
+            });
+        done();
     });
-    done()
+
+    test('NOT FOUND', async done => {
+        await request
+            .post('/account/0')
+            .send(data)
+            .then( response => {
+                expect(response.status).toBe(404);
+                expect(response.body.info).toBeUndefined();
+            });
+        done();
+    });
+
+    test('BAD REQUEST', async done => {
+        await request
+            .post('/account/abc')
+            .send(data)
+            .then( response => {
+                expect(response.status).toBe(400);
+                expect(response.body.info).toBeUndefined();
+            });
+        done();
+    });
+})
+
+describe('ACCOUNT::ID::PASSWORD', () => {
+
+    test('POST::NEW PASS::GOOD', async done => {
+        await request.post('/account/2/password_reset').then( response => {
+            expect(response.status).toBe(200);
+            expect(response.body.info.account_id).toBe(2);
+        });
+        done()
+    });
+
+    test('POST::NEW PASS::BAD REQUEST', async done => {
+        await request.post('/account/abc/password_reset').then( response => {
+            expect(response.status).toBe(400);
+            expect(response.body.info).toBeUndefined();
+        });
+        done()
+    });
+
+    test('POST::NEW PASS::NOT FOUND', async done => {
+        await request.post('/account/0/password_reset').then( response => {
+            expect(response.status).toBe(404);
+            expect(response.body.info).toBeUndefined();
+        });
+        done()
+    });
+})
+
+
+describe('ACCOUNT::CHECK EMAILS', () => {
+
+    test('Email Exists', async done => {
+        await request.get('/account/check')
+            .query({ email: 'example2@example.com' })
+            .then( response => {
+                expect(response.status).toBe(200);
+                expect(response.body.info).toBe(1);
+        })
+        done()
+    })
+
+    test('Email Does Not Exist', async done => {
+        await request.get('/account/check')
+            .query({ email: 'example0@example.com' })
+            .then( response => {
+                expect(response.status).toBe(404);
+                expect(response.body.info).toBe(0);
+        })
+        done()
+    })
+
+});
+
+describe('ACCOUNT::CREATE THEN DELETE', () => {
+    test('ALl PARAMS PRESENT', async done => {
+        const data = {
+            email: 'temp2@temp.com',
+            fname: 'tempname',
+            lname: 'templname',
+            pass: '$2b$10$SMOfe5vCHGYurgnVfIyTpOn9/IVF1IpJtDHR3SdHk2eeiFEheNMSm'
+        }
+        await request.post('/account/create')
+            .send(data)
+            .then( async response => {
+                expect(response.status).toBe(200);
+                expect(response.body.info.account_id).toBeDefined();
+
+                await request.delete('/account/'+response.body.info.account_id+'/delete')
+                    .then( resp => {
+                        expect(resp.status).toBe(200);
+                        expect(resp.body.info.account_id).toBe(response.body.info.account_id);
+                    })
+            })
+        done();
+    })
 });
